45a9c1bc0f33e5da0db0fe68e5a3e275dc20b39d
Javier Requejo <javier@alternaenergetica.com>
2020-01-13 20:34:18 +0100

    Setting up Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Javi Requejo Portfolio</h1>
+        </div>
+    )
+}
+
+export default IndexPage

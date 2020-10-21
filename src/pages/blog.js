50a8f5c1b3f0629c064182e0a5bf941d75cda438
Javier Requejo <javier@alternaenergetica.com>
2020-10-21 23:49:45 +0100

    Added theme to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import mainTheme from '../styles/theme';
+import GlobalStyle from '../styles/styledComponents/GlobalStyle';
 import Layout from '../components/Common/Layout';
 import PostsContainer from '../components/Blog/PostsContainer';
 
 const Blog = () => (
-  <Layout>
-    <h1>Blog</h1>
-    <PostsContainer />
-  </Layout>
+  <ThemeProvider theme={mainTheme}>
+    <GlobalStyle />
+    <Layout>
+      <h1>Blog</h1>
+      <PostsContainer />
+    </Layout>
+  </ThemeProvider>
 );
 
 export default Blog;

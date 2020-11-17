c039e966e32ba10b785ec8eb36873e3c9dbee883
Javier Requejo <javier@alternaenergetica.com>
2020-11-17 14:29:49 +0000

    Testing google analytics

---
+++ src/html.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/html-has-lang */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TV8BP6V9DE"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TV8BP6V9DE');
+        `,
+          }}
+        />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
